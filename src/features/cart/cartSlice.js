000d64517d26db739737a83e58bcc48d1afc1416
Matt Engerer <matt.engerer@aol.com>
2022-07-26 17:25:51 -0400

    redux tutorial underway

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -4,7 +4,7 @@ import cartItems from '../../cartItems'
 
 const initialState = {
     cartItems: cartItems,
-    amount: 5,
+    amount: 2,
     total: 0,
     isLoading: true
 }
@@ -23,12 +23,22 @@ const cartSlice = createSlice({
                 return item.id !== itemId
             })
                 console.log(state.cartItems)
+        },
+        increase: (state, {payload} ) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload)
+            cartItem.amount = cartItem.amount + 1;
+            console.log(cartItem.amount)
+
+        },
+        decrease: (state, {payload} ) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
+            cartItem.amount = cartItem.amount - 1 
         }
     }
 }) 
 
 console.log(cartSlice)
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer
