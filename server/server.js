7c7009bb466c22387cd0d6d4c729db119e714ab5
Matt Engerer <matt.engerer@aol.com>
2022-07-27 01:44:03 -0400

    added graphQL backend - not connected to front yet

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const {gql, ApolloServer} = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`;
+
+  const resolvers = {
+    Query: {
+      greeting: () => 'Hello GraphQL world!👋',
+    },
+  };
+  
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at ${url}`));
